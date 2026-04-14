381868aa3e668061105e3ec223a6b1b9c6f73820
Shubham <68602224+Shubham-Chorage@users.noreply.github.com>
2026-04-13 22:21:56 -0500

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        
+        count = {}
+
+        for num in hand:
+            count[num] = count.get(num,0) + 1
+
+        if len(hand) % groupSize != 0:
+            return False
+
+        for num in sorted(count):
+            while count[num] > 0:
+                for i in range(groupSize):
+                    if count.get(num + i, 0) == 0:
+                        return False
+                    count[num + i] -= 1
+
+        return True
