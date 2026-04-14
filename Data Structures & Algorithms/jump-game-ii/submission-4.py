ceda218fb86863e9d77071f6a4c9fddafefbc9c0
Shubham <68602224+Shubham-Chorage@users.noreply.github.com>
2026-04-14 11:27:38 -0500

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        jumps = 0
+        curr_max = 0
+        curr_end = 0
+
+        for i in range(len(nums)-1):
+            curr_max = max(curr_max, nums[i] + i)
+
+            if i == curr_end:
+                jumps += 1
+                curr_end = curr_max
+
+        return jumps
