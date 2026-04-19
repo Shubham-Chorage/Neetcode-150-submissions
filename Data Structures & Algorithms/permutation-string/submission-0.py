174280acabb19fff5518ca212c0d7f55c78400a7
Shubham <68602224+Shubham-Chorage@users.noreply.github.com>
2026-04-18 22:00:32 -0500

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,54 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        if len(s1) > len(s2):
+            return False
+
+        count1 = [0] * 26
+        count2 = [0] * 26
+
+        for i in range(len(s1)):
+            count1[ord(s1[i]) - ord('a')] += 1
+            count2[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+
+        for i in range(26):
+            if count1[i] == count2[i]:
+                matches += 1
+
+        if matches == 26:
+            return True
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+
+            idx = ord(s2[r]) - ord('a')
+            # BEFORE update comparison effect
+            if count2[idx] == count1[idx]:
+                matches -= 1
+
+            count2[idx] += 1
+
+            if count2[idx] == count1[idx]:
+                matches += 1
+
+            # remove left character
+            idx = ord(s2[l]) - ord('a')
+
+            if count2[idx] == count1[idx]:
+                matches -= 1
+
+            count2[idx] -= 1
+
+            if count2[idx] == count1[idx]:
+                matches += 1
+
+            l += 1
+
+            if matches == 26:
+                return True
+
+        return False
+
+        
